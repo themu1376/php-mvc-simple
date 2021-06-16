77be68fc961993bb06112b0fd460105bd7225285
HyoSung <themu@naver.com>
2021-06-16 23:29:06 +0900

    add: Tag class

--- application/config/lib.php
+++ application/config/lib.php
@@ -47,3 +47,54 @@
 	    }
 	    return $randomString;
 	}
+	
+	function tag($name, $properties=[]) {
+		return new Tag($name, $properties);
+	}
+	class Tag {
+		var $name;
+		var $children;
+		public function __construct($name, $properties=[]) {
+			$this->name = $name;
+			$this->children = array();
+			print("<{$name}");
+			if(is_array($properties)) {
+				foreach($properties as $i => $property) {
+					if(is_array($property)) {
+						foreach($property as $key => $value) {
+							print(" $key=\"$value\"");
+						}
+					}
+					else {
+						print(' '.$property);
+					}
+				}
+			}
+			else {
+				print(' '.$properties);
+			}
+			print(">");
+		}
+		public function __destruct() {
+			print("</{$this->name}>");
+		}
+		public function close() {
+			unset($this);
+		}
+		public function text($val) {
+			print($val);
+			return $this;
+		}
+		public function addChild($obj) {
+			array_push($this->children, $obj);
+			return $this;
+		}
+		public function call($func, $params) {
+			$func($params);
+			return $this;
+		}
+		public function debug() {
+			printArray($this);
+		}
+	}
+	
